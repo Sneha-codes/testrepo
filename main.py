de8fea9034fabeadb768d8839bc3674cf1412311
Sneha-codes <74462732+Sneha-codes@users.noreply.github.com>
2021-12-20 21:00:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+f=open("file")
+for i in range(0,5):
+    name=input("enter name")
+    stu={i:name};
+pickle.dump(Stu.f)
+f.close()
+f.open()
+s={}
+while True:
+    try:
+        t=pickle.load(f)
+        for i in t:
+            for j in range i.size
+    except EOFError:
+        break
+f.close()
+print(s)
+
+for i in x:
+    p=input()
